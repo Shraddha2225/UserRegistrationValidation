e810e9619a821ae4df16b636a3fc9a267597af7c
Shraddha2225 <shraddhat22m@gmail.com>
2020-02-17 16:05:54 +0530

    ADDED: Welcome Message

---
+++ src/main/java/com/bridgelab/demo/UserValidation.java
@@ -0,0 +1,8 @@
+package com.bridgelab.demo;
+import java.util.regex.Pattern;
+public class UserValidation{
+    public static void main(String[] args) {
+       System.out.println("WELCOME TO USER REGISTRATION");
+    }
+}
+
---
+++ src/test/java/com/bridgelab/demo/UserRegistrationTest.java
@@ -0,0 +1,8 @@
+package com.bridgelab.demo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserRegistrationTest {
+
+}
